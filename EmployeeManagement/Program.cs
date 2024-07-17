179ff4b06f037f3f00cec4ecb35ae3eb94159eeb
RioKennedy <riokennedy135@gmail.com>
2024-07-17 22:21:59 +0800

    Build Request Response PipeLine Middleware

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,9 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+ILogger logger = app.Logger;
 
 
 IConfiguration configuration = app.Configuration;
-app.MapGet("/", () => configuration["MyKey"]);
+app.Use(async (a,next) => 
+{
+    logger.LogInformation("MW1: Incoming Request");
+    await next();
+    logger.LogInformation("MW1: Outgoing Response");
+});
 
+app.Use(async (a,next) => 
+{
+    logger.LogInformation("MW2: Incoming Request");
+    await next();
+    logger.LogInformation("MW2: Outgoing Response");
+});
 
+app.Run(async a => 
+{
+    await a.Response.WriteAsync("MW3: Request handled and response produced");
+    logger.LogInformation("MW3: Request handled and response");
+});
 app.Run();
