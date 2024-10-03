d22783fed7a0852e4b2e828b4ccc0136d1330172
RioKennedy <riokennedy135@gmail.com>
2024-10-03 19:31:04 +0800

    Commit

--- EmployeeManagement/obj/Debug/net7.0/EmployeeManagement.AssemblyInfo.cs
+++ EmployeeManagement/obj/Debug/net7.0/EmployeeManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86959ebaf8f864b873267cf1eedb26664fbc6283")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7f1dfe9b76b263c7019b6409622e96ba6ca1777")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
